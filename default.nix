09cdb6adc48efc523983d790c33bdc6b10ecc384
Tobias Mayer <tobim@fastmail.fm>
2018-03-21 22:27:54 +0100

    use better version of clang stdenvs; add libcxx variants

--- default.nix
+++ default.nix
@@ -9,12 +9,19 @@ let
   gcc6Stdenv = super.overrideCC super.stdenv super.gcc6;
   gcc7Stdenv = super.overrideCC super.stdenv super.gcc7;
 
-  clang36Stdenv = super.overrideCC super.clangStdenv super.clang_36;
-  clang37Stdenv = super.overrideCC super.clangStdenv super.clang_37;
-  clang38Stdenv = super.overrideCC super.clangStdenv super.clang_38;
-  clang39Stdenv = super.overrideCC super.clangStdenv super.clang_39;
-  clang4Stdenv = super.overrideCC super.clangStdenv super.clang_4;
-  clang5Stdenv = super.overrideCC super.clangStdenv super.clang_5;
+  clang35Stdenv = super.llvmPackages_35.stdenv;
+  clang37Stdenv = super.llvmPackages_37.stdenv;
+  clang38Stdenv = super.llvmPackages_38.stdenv;
+  clang39Stdenv = super.llvmPackages_39.stdenv;
+  clang4Stdenv = super.llvmPackages_4.stdenv;
+  clang5Stdenv = super.llvmPackages_5.stdenv;
+
+  clang35libcxxStdenv = super.llvmPackages_35.libcxxStdenv;
+  clang37libcxxStdenv = super.llvmPackages_37.libcxxStdenv;
+  clang38libcxxStdenv = super.llvmPackages_38.libcxxStdenv;
+  clang39libcxxStdenv = super.llvmPackages_39.libcxxStdenv;
+  clang4libcxxStdenv = super.llvmPackages_4.libcxxStdenv;
+  clang5libcxxStdenv = super.llvmPackages_5.libcxxStdenv;
 
 in
 {
@@ -26,12 +33,19 @@ in
   gcc6pkgs = import ./c++libs-overlay.nix { stdenv = gcc6Stdenv; } self super;
   gcc7pkgs = import ./c++libs-overlay.nix { stdenv = gcc7Stdenv; } self super;
 
-  clang36pkgs = import ./c++libs-overlay.nix { stdenv = clang36Stdenv; } self super;
+  clang35pkgs = import ./c++libs-overlay.nix { stdenv = clang35Stdenv; } self super;
   clang37pkgs = import ./c++libs-overlay.nix { stdenv = clang37Stdenv; } self super;
   clang38pkgs = import ./c++libs-overlay.nix { stdenv = clang38Stdenv; } self super;
   clang39pkgs = import ./c++libs-overlay.nix { stdenv = clang39Stdenv; } self super;
   clang4pkgs = import ./c++libs-overlay.nix { stdenv = clang4Stdenv; } self super;
   clang5pkgs = import ./c++libs-overlay.nix { stdenv = clang5Stdenv; } self super;
+
+  clang35libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang35libcxxStdenv; } self super;
+  clang37libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang37libcxxStdenv; } self super;
+  clang38libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang38libcxxStdenv; } self super;
+  clang39libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang39libcxxStdenv; } self super;
+  clang4libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang4libcxxStdenv; } self super;
+  clang5libcxxpkgs = import ./c++libs-overlay.nix { stdenv = clang5libcxxStdenv; } self super;
 }
 
 
